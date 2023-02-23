fcd70ab1f35b463b79d2c6141b5b81f9e64a4454
iemong <kiyoshidainagon@gmail.com>
2023-02-23 23:45:57 +0900

    add rules

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 module.exports = {
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', "plugin:import/typescript"],
-    extends: ["standard-with-typescript", "prettier"],
-    rules: {
-        "no-console": 1,
-    }
-}
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["import", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:promise/recommended",
+    "prettier",
+  ],
+  rules: {
+    "import/order": 2,
+    "import/no-default-export": 2,
+    "no-new-object": 2,
+    "no-new-func": 2,
+    "@typescript-eslint/consistent-type-imports": [
+      2,
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+  },
+};
